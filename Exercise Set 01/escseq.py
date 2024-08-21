2819b815cbbd3545f0fa11bbfcb3adc59dded62f
Gettie <saucegettie@gmail.com>
2024-08-21 11:22:33 +0800

    001 | Exercise 1, Aug 21

---
+++ Exercise Set 01/escseq.py
@@ -0,0 +1,5 @@
+#exercise 2
+for x in range(7, 42, 7):
+    if x % 2 == 0:
+        print("\t", x)
+    else: print(x) 
---
+++ Exercise Set 01/tempconvert.py
@@ -0,0 +1,4 @@
+#exercise 3
+cel = input("Enter a Celsius value: ")  
+formula = float(cel) * 9/5 + 32  
+print(format(float(cel), '.2f'), "Celsius is", format(formula, '.2f'), "Fahrenheit")
