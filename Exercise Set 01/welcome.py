77da96c59b58160f64b2cdba15e0055cb2ddfa95
Gettie <saucegettie@gmail.com>
2024-08-21 11:27:26 +0800

    002 | deleted exercises 2 and 3 in file 1

--- Exercise Set 01/welcome.py
+++ Exercise Set 01/welcome.py
@@ -3,15 +3,3 @@ name = input("Enter your name: ")
 print("This course will be fun, " +name+ "!" )
 
 
-#exercise 2
-for x in range(7, 42, 7):
-    if x % 2 == 0:
-        print("\t", x)
-    else: print(x) 
-
-#exercise 3
-#formula (0°C × 9/5) + 32 
-
-cel = input("Enter a Celsius value: ")
-formula = float(cel) * 9/5+32
-print( cel + " Celsius is ", formula, " Fahrenheit" )
