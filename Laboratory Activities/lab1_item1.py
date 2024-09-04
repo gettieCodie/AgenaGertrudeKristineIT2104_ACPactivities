536bd3874c483777360a5b7545a01865168be872
Gettie <saucegettie@gmail.com>
2024-09-04 10:36:29 +0800

    003 | Sept 4, 2024(LabAct1)

---
+++ Laboratory Activities/lab1_item1.py
@@ -0,0 +1,14 @@
+year = input("Enter the year: ").strip()
+year2 = int(year)
+genre = input("Enter the genre: ").strip()
+album = input("Enter the album: ").strip()
+songTitle = input("Enter the song title: ").strip()
+artist = input("Enter the artist: ").strip()
+
+print("---------------------\nSONG DETAILS\n---------------------")
+
+print(f"Year Release: {year2}")
+print(f"Genre: {genre}")
+print(f"Album: {album}")
+print(f"Title: {songTitle}")
+print(f"Artist: {artist}")
---
+++ Laboratory Activities/lab2_item2.py
@@ -0,0 +1,3 @@
+charOne, charTwo = input("Enter two space-separated characters: ").split()
+greaterVal = max(charOne, charTwo)
+print(f"The character with greater value is: {greaterVal}")
