9128ef5496037d1bae3cd35a7ec047d16c9353d8
Gettie <saucegettie@gmail.com>
2024-09-04 11:11:20 +0800

    003 | Sept 4, 2024 | LabAct1

--- Laboratory Activities/Laboratory Activity 1 -  04-September-2024/lab1_item2.py
+++ Laboratory Activities/Laboratory Activity 1 -  04-September-2024/lab1_item2.py
@@ -1,3 +1,5 @@
 charOne, charTwo = input("Enter two space-separated characters: ").split()
 greaterVal = max(charOne, charTwo)
+print("---------------------------------------")
+
 print(f"The character with greater value is: {greaterVal}")
